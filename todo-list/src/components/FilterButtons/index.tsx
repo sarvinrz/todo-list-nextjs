6d73420dbcfbb5f7cdcc38803da555c6ace3cb3e
sarvinrz <sarvin.rezazadeh7979@gmail.com>
2024-04-14 07:37:06 +0330

    add filter buttuns component

---
+++ todo-list/src/components/FilterButtons/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '@/styles/Home.module.css';
+
+const FilterButtons = function () {
+  return (
+    <main className={`${styles.main}`}>
+      <div className='filter-buttuns'>hello</div>
+    </main>
+  );
+};
+
+export default React.memo(FilterButtons);
